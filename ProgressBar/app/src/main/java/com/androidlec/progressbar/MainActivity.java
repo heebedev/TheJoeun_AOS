9ff6d5f0cb928c8ad10286aecc02775c9a2bc76e
TJ <tj@HONG.local>
2020-06-26 10:53:04 +0900

    안드수업 프로그래스바 마감

--- ProgressBar/app/src/main/java/com/androidlec/progressbar/MainActivity.java
+++ ProgressBar/app/src/main/java/com/androidlec/progressbar/MainActivity.java
@@ -1,14 +1,76 @@
+
 package com.androidlec.progressbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ProgressBar pb01, pb02;
+    Button progressBtn, resetBtn, showBtn, hideBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pb01 = findViewById(R.id.pb_01);
+        pb02 = findViewById(R.id.pb_02);
+
+        progressBtn = findViewById(R.id.btn_01);
+        resetBtn = findViewById(R.id.btn_02);
+        showBtn = findViewById(R.id.btn_03);
+        hideBtn = findViewById(R.id.btn_04);
+
+
+        progressBtn.setOnClickListener(clickListner);
+        resetBtn.setOnClickListener(clickListner);
+        showBtn.setOnClickListener(clickListner);
+        hideBtn.setOnClickListener(clickListner);
+
+    } // OnCreate
+
+
+
+    View.OnClickListener clickListner = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            switch (v.getId()) {
+                case R.id.btn_01 :
+                    btn_01();
+                    break;
+                case R.id.btn_02 :
+                    Toast.makeText(MainActivity.this, "Reset", Toast.LENGTH_SHORT).show();
+                    pb01.setProgress(0);
+                    pb01.setSecondaryProgress(50);
+                    break;
+                case R.id.btn_03 :
+                    Toast.makeText(MainActivity.this, "Show", Toast.LENGTH_SHORT).show();
+                    pb02.setVisibility(ProgressBar.VISIBLE);
+                    break;
+                case R.id.btn_04 :
+                    Toast.makeText(MainActivity.this, "hide", Toast.LENGTH_SHORT).show();
+                    pb02.setVisibility(ProgressBar.INVISIBLE);
+                    break;
+
+            }
+
+
+
+        }
+    };
+
+    private void btn_01() {
+        Toast.makeText(MainActivity.this, "Progress", Toast.LENGTH_SHORT).show();
+        pb01.incrementProgressBy(10);
+        pb01.incrementSecondaryProgressBy(10);
     }
-}
+
+} // main
